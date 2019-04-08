8485a3b1accbf5dcd7cbffa5c400dc561174c739
karakum <zhangjianxing2011@gmail.com>
2019-04-08 12:13:30 +0800

    first commit

---
+++ src/com/sicher/ctrl/Test.java
@@ -0,0 +1,9 @@
+package com.sicher.ctrl;
+
+public class Test {
+	private Long id = 1L;
+	private Long iq = 2L;
+
+	private Long eq = 3L;
+	private Long num = 4L;
+}
---
+++ src/com/sicher/service/Service.java
@@ -0,0 +1,6 @@
+package com.sicher.service;
+
+public class Service {
+
+	private String username
+}
